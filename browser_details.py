c39cdf9b96549d05dc1c4d2f4c21b3bddbfa3cd0
Abhishek <abhi1631shek.com>
2024-11-12 20:55:35 -0600

    fixed pdf_viewing

---
+++ browser_details.py
@@ -0,0 +1,16 @@
+import streamlit as st
+from streamlit_js_eval import get_user_agent
+from user_agents import parse
+
+def get_browser():
+    # Retrieve the user agent
+    user_agent_str = get_user_agent()
+
+    # Parse the user agent string
+    user_agent = parse(user_agent_str)
+
+    # Extract browser information
+    browser_name = user_agent.browser.family
+
+    # return details
+    return browser_name
