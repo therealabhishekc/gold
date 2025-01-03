a897247c7962ffbec362900f20621261af891a51
Abhishek <abhi1631shek.com>
2025-01-03 00:23:06 -0600

    test

---
+++ test.py
@@ -0,0 +1,15 @@
+
+import streamlit as st
+import base64
+
+st.write("HIUIII")
+
+# Path to your PDF file (make sure the file exists and is accessible)
+pdf_file_path = "resume.pdf"  # Replace with your PDF file path or URL
+
+with open(pdf_file_path, "rb") as f:
+        base64_pdf = base64.b64encode(f.read()).decode('utf-8')
+
+pdf_display = F'<iframe src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf"></iframe>'
+
+st.markdown(pdf_display, unsafe_allow_html=True)
