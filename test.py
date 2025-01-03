b69bbec013d68404d480569166718f3b6124ab23
Abhishek <abhi1631shek.com>
2025-01-03 00:30:44 -0600

    'test'

--- test.py
+++ test.py
@@ -10,6 +10,10 @@ pdf_file_path = "resume.pdf"  # Replace with your PDF file path or URL
 with open(pdf_file_path, "rb") as f:
         base64_pdf = base64.b64encode(f.read()).decode('utf-8')
 
-pdf_display = F'<iframe src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf"></iframe>'
+pdf_display = F'<embed src="data:application/pdf;base64,{base64_pdf}" 
+                        width="700" 
+                        height="1000" 
+                        type="application/pdf">
+                </embed>'
 
 st.markdown(pdf_display, unsafe_allow_html=True)
